5a11fe5ac19482026fcb5427101f26d7b6a75bfd
ashok <ashokchakravarthi12@gmail.com>
2022-08-25 21:53:45 +0800

    Complete unittesting for accounts

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/cashok08/dream-bank/util"
@@ -9,7 +10,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+//This func insert a record into accounts table
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -26,4 +28,83 @@ func TestCreateAccount(t *testing.T) {
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	//Insert a record in accounts table
+	account1 := createRandomAccount(t)
+	// Read back the inserted row
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	//Check the error is nill
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	//All the fields of account1 should be equal to account2
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	//Timezone of insertion should be less than a sec
+
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, 1)
+
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, 1)
+
+	//Balance should be equal to updated value
+	require.Equal(t, arg.Balance, account2.Balance)
+
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	//Extra Check
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Empty(t, account2)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+}
+
+func TestListAccount(t *testing.T) {
+
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
